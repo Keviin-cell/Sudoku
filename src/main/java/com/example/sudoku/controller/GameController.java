6b5522146247e7384319bd8d4e17548eb9191e1b
kevin <kevinmunoz6002@gmail.com>
2025-04-08 22:25:58 -0500

    Add model classes: Board, Celda, and Sudoku with basic implementations

---
+++ src/main/java/com/example/sudoku/controller/GameController.java
@@ -0,0 +1,13 @@
+package com.example.sudoku.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class GameController {
+
+    @FXML
+    private TextField celda;
+
+
+}
